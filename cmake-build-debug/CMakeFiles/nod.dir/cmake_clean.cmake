d40ee7fc10d2d69bd524136f5ac8aa8c7ebbdced
Hubert Badocha <badocha.hubert@gmail.com>
2020-10-19 14:22:28 +0200

    setup

---
+++ cmake-build-debug/CMakeFiles/nod.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nod.dir/nod.cc.o"
+  "nod"
+  "nod.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nod.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
