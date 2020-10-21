80265cbfc21c64f4df48eafc434f2534e4213e5b
Hubert Badocha <badocha.hubert@gmail.com>
2020-10-21 21:05:41 +0200

    created function stubs

--- nod.cc
+++ nod.cc
@@ -1,6 +1,38 @@
 #include <iostream>
 
+enum class RoadType {
+    HIGHWAY = "A", EXPRESSWAY = "S"
+};
+
+using LicensePlate = string;
+using RoadNumber = int;
+using RoadDistancePost = int;
+
+using Memory = map <RoadType, map<RoadNumber, map < LicensePlate, vector < int>>>>; // may change in future but doesn't really matter rn
+
+
+void log(LicensePlate &licensePlate,
+         RoadType roadType,
+         RoadNumber roadNumber,
+         RoadDistancePost roadDistancePost,
+         Memory &memory) {
+
+}
+
+void general_query(Memory &memory) {
+
+}
+
+void query_car(LicensePlate *licensePlate, Memory &memory) {
+
+}
+
+void query_road(RoadType roadType, RoadNumber roadNumber, Memory &memory) {
+
+}
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+    Memory memory;
+
+    return 0;
 }
